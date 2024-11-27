5769e69c96bde522158277a859d305e47f7af0f8
Petersaad5 <152736305+Petersaad5@users.noreply.github.com>
2024-11-27 10:01:12 +0100

    first commit

---
+++ src/main/java/com/optimodlyon/optimodlyon/OptimodLyonApplication.java
@@ -0,0 +1,13 @@
+package com.optimodlyon.optimodlyon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimodLyonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptimodLyonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/optimodlyon/optimodlyon/OptimodLyonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optimodlyon.optimodlyon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptimodLyonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
