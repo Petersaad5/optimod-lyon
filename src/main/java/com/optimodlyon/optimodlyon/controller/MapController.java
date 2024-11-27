f0d97a1884d2ba4184513d3f0a971b93c91fc0e4
Petersaad5 <petersaad555@gmail.com>
2024-11-27 23:08:28 +0100

    Added map data api endpoints

--- src/main/java/com/optimodlyon/optimodlyon/controller/MapController.java
+++ src/main/java/com/optimodlyon/optimodlyon/controller/MapController.java
@@ -1,4 +1,40 @@
 package com.optimodlyon.optimodlyon.controller;
 
+import com.optimodlyon.optimodlyon.model.Data;
+import com.optimodlyon.optimodlyon.model.IntersectionModel;
+import com.optimodlyon.optimodlyon.model.MapModel;
+import com.optimodlyon.optimodlyon.model.RoadModel;
+import com.optimodlyon.optimodlyon.service.MapService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/map")
 public class MapController {
+    private final MapService mapService;
+
+    @Autowired
+    public MapController(MapService mapService) {
+        this.mapService = mapService;
+    }
+
+
+    @GetMapping
+    public MapModel getMapData() {
+        return mapService.getMapData();
+    }
+
+    @GetMapping("/intersections")
+    public List<IntersectionModel> getIntersections() {
+        return mapService.getIntersections();
+    }
+
+    @GetMapping("/roads")
+    public List<RoadModel> getRoads() {
+        return mapService.getRoads();
+    }
 }
--- src/main/java/com/optimodlyon/optimodlyon/service/MapService.java
+++ src/main/java/com/optimodlyon/optimodlyon/service/MapService.java
@@ -1,4 +1,39 @@
 package com.optimodlyon.optimodlyon.service;
 
+import com.optimodlyon.optimodlyon.model.Data;
+import com.optimodlyon.optimodlyon.model.IntersectionModel;
+import com.optimodlyon.optimodlyon.model.MapModel;
+import com.optimodlyon.optimodlyon.model.RoadModel;
+import com.optimodlyon.optimodlyon.utils.Parser;
+import org.springframework.stereotype.Service;
+
+import jakarta.annotation.PostConstruct;
+
+import java.util.List;
+
+@Service
 public class MapService {
+    private Data data;
+
+    @PostConstruct
+    public void init() {
+        // Parse the XML files and load the data
+        Parser.parsePlan("src/public/xml/grandPlan.xml");
+        Parser.parseDemande("src/public/xml/demandeGrand7.xml");
+        this.data = Parser.data;
+    }
+
+    public MapModel getMapData() {
+        return data.getMap();
+    }
+
+    public List<IntersectionModel> getIntersections() {
+        return data.getIntersections();
+    }
+
+    public List<RoadModel> getRoads() {
+        return data.getRoads();
+    }
+
+
 }
